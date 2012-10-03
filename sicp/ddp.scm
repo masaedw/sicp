050ff43ce35a39cfb6829f47506c341fc9522049
masaedw <masa.edw@gmail.com>
2012-10-04 07:21:24 +0900

    2.81: p.115 apply-generic

--- sicp/ddp.scm
+++ sicp/ddp.scm
@@ -43,8 +43,21 @@
     (let ((proc (get-method op type-tags)))
       (if proc
           (apply proc (map contents args))
-          (error
-           "No method for these types -- APPLY-GENERIC"
-           (list op type-tags))))))
+          (if (= (length args) 2)
+              (let ([type1 (car type-tags)]
+                    [type2 (cadr type-tags)]
+                    [a1 (car args)]
+                    [a2 (cadr args)])
+                (let ([t1->t2 (get-coercion type1 type2)]
+                      [t2->t1 (get-coercion type2 type1)])
+                  (cond [t1->t2
+                         (apply-generic op (t1->t2 a1) a2)]
+                        [t2->t1
+                         (apply-generic op a1 (t2->t1 a2))]
+                        [else
+                         (error "No method for these types"
+                                (list op type-tags))])))
+              (error "No method for these types"
+                     (list op type-tags)))))))
 
 (provide "sicp/ddp")
