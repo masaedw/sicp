1b0fc66c4cfe2e0fbb0cb26dd59b5351851efe27
masaedw <masa.edw@gmail.com>
2012-10-15 23:29:44 +0900

    fix indent

--- sicp/ddp.scm
+++ sicp/ddp.scm
@@ -42,25 +42,25 @@
   (let ((type-tags (map type-tag args)))
     (let ((proc (get-method op type-tags)))
       (if proc
-          (apply proc (map contents args))
-          (if (= (length args) 2)
-              (let ([type1 (car type-tags)]
-                    [type2 (cadr type-tags)]
-                    [a1 (car args)]
-                    [a2 (cadr args)])
-                (if (eq? type1 type2)
-                  (error "No method for these types" (list op type-tags))
-                  (begin
-                    (let ([t1->t2 (get-coercion type1 type2)]
-                          [t2->t1 (get-coercion type2 type1)])
-                      (cond [t1->t2
-                             (apply-generic op (t1->t2 a1) a2)]
-                            [t2->t1
-                             (apply-generic op a1 (t2->t1 a2))]
-                            [else
-                             (error "No method for these types"
-                               (list op type-tags))])))))
-              (error "No method for these types"
-                     (list op type-tags)))))))
+        (apply proc (map contents args))
+        (if (= (length args) 2)
+          (let ([type1 (car type-tags)]
+                [type2 (cadr type-tags)]
+                [a1 (car args)]
+                [a2 (cadr args)])
+            (if (eq? type1 type2)
+              (error "No method for these types" (list op type-tags))
+              (begin
+                (let ([t1->t2 (get-coercion type1 type2)]
+                      [t2->t1 (get-coercion type2 type1)])
+                  (cond [t1->t2
+                         (apply-generic op (t1->t2 a1) a2)]
+                        [t2->t1
+                         (apply-generic op a1 (t2->t1 a2))]
+                        [else
+                         (error "No method for these types"
+                           (list op type-tags))])))))
+          (error "No method for these types"
+            (list op type-tags)))))))
 
 (provide "sicp/ddp")
