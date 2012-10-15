56591c0bc5b67a6df4576c314f978a164eb723bd
masaedw <masa.edw@gmail.com>
2012-10-16 05:36:02 +0900

    fix comment of apply-generic

--- sicp/ddp.scm
+++ sicp/ddp.scm
@@ -46,27 +46,8 @@
         [else
          (error "Bad tagged datum -- CONTENTS" datum)]))
 
-#;(if (= (length args) 2)
-  (let ([type1 (car type-tags)]
-        [type2 (cadr type-tags)]
-        [a1 (car args)]
-        [a2 (cadr args)])
-    (if (eq? type1 type2)
-      (error "No method for these types" (list op type-tags))
-      (begin
-        (let ([t1->t2 (get-coercion type1 type2)]
-              [t2->t1 (get-coercion type2 type1)])
-          (cond [t1->t2
-                 (apply-generic op (t1->t2 a1) a2)]
-                [t2->t1
-                 (apply-generic op a1 (t2->t1 a2))]
-                [else
-                 (error "No method for these types"
-                   (list op type-tags))])))))
-  (error "No method for these types"
-    (list op type-tags)))
-
-;; 2.82 すべての引数を第一引数の型、次に第二引数の型等々に強制変換を試みる実装
+;; 2.82 による
+;; すべての引数を第一引数の型、次に第二引数の型等々に強制変換を試みる実装
 ;; これは十分に一般的ではない。
 ;;
 ;; 例:
