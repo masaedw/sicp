d51391800058e2ef899b752d36bfd6cdf4f88f95
masaedw <masa.edw@gmail.com>
2012-10-30 00:56:21 +0900

    Add plolynomial package

--- sicp/ddp.scm
+++ sicp/ddp.scm
@@ -385,3 +385,97 @@
 (install-rectangular-package)
 (install-polar-packge)
 (install-complex-package)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 多項式パッケージ
+
+(define (install-polynomial-package)
+  ;; 内部手続き
+  ;; 多項式型の表現
+
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+  (define (variable p) (car p))
+  (define (term-list p) (cdr p))
+
+  (define (variable? x) (symbol? x))
+
+  (define (same-variable? v1 v2)
+    (and (variable? v1) (variable? v2) (eq? v1 v2)))
+
+  (define (the-empty-term-list) ())
+  (define (empty-termlist? term-list) (null? term-list))
+
+  (define (adjoin-term term term-list)
+    (if (=zero? (coeff term))
+      term-list
+      (cons term (term-list))))
+
+  (define (first-term term-list) (car term-list))
+  (define (rest-terms term-list) (cdr term-list))
+  (define (make-term order coeff) (list order coeff))
+  (define (order term) (car term))
+  (define (coeff term) (cdr term))
+
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (add-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polyqs not in same var -- ADD-POLY"
+        (list p1 p2))))
+
+  (define (add-terms L1 L2)
+    (cond [(empty-termlist? L1) L2]
+          [(empty-termlist? L2) L1]
+          [else
+           (let ([t1 (first-term L1)]
+                 [t2 (first-term L2)])
+             (cond [(> (order t1) (order t2))
+                    (adjoin-term t1
+                                 (add-terms (rest-terms L1) L2))]
+                   [(< (order t1) (order t2))
+                    (adjoin-term t2
+                                 (add-terms L1 (rest-terms L2)))]
+                   [else
+                    (adjoin-term (make-term (order t1)
+                                            (add (coeff t1) (coeff t2)))
+                                 (add-terms (rest-terms L1)
+                                            (rest-terms L2)))]))]))
+
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (mul-terms (term-list p1)
+                            (term-list p2)))
+      (error "Polys not in same var -- MUL-POLY"
+        (list p1 p2))))
+
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+      (the-empty-term-list)
+      (add-terms (mul-term-by-all-terms (first-term L1) L2)
+                 (mul-terms (rest-terms L1) L2))))
+
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+      (the-empty-term-list)
+      (let ([t2 (first-term L)])
+        (adjoin-term (make-term (+ (order t1) (order t2))
+                                (mul (coeff t1) (coeff t2)))
+                     (mul-term-by-all-terms t1 (rest-terms L))))))
+
+  ;; システムの他の部分とのインターフェース
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (^ (p1 p2) (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (^ (p1 p2) (tag (mul-poly p1 p2))))
+  (put 'make 'polynomial
+       (^ (var terms) (tag (make-poly var terms))))
+
+  'done)
+
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
