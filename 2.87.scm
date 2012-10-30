70f82798fdf8750e5761aeddd467f3bc255582f9
masaedw <masa.edw@gmail.com>
2012-10-31 00:39:13 +0900

    Fix plolynomial package

---
+++ 2.87.scm
@@ -0,0 +1,28 @@
+(add-load-path ".")
+(use sicp.ddp)
+(use gauche.test)
+
+;; 過去の実装を壊してないこと
+(load "2.85")
+
+(test-start "2.87")
+
+(test* "add polynomials"
+       (make-polynomial 'x '((3 5) (2 8) (1 1) (0 2)))
+       (add (make-polynomial 'x '((2 1) (1 1) (0 1)))
+            (make-polynomial 'x '((3 5) (2 7) (0 1))))
+       equal?) ;; equ? はこの段階では未定義
+
+(test* "add polynomials with other numeric types"
+       (make-polynomial 'x `((3 5) (2 ,(make-rational 18 5)) (1 1) (0 2)))
+       (add (make-polynomial 'x `((2 ,(make-rational 3 5)) (1 1) (0 1)))
+            (make-polynomial 'x '((3 5) (2 3) (0 1))))
+       equal?) ;; equ? はこの段階では未定義
+
+
+(test* "mul polynomials"
+       (make-polynomial 'x '((5 5) (4 5) (2 1) (1 1)))
+       (mul (make-polynomial 'x '((2 1) (1 1)))
+            (make-polynomial 'x '((3 5) (0 1))))
+       equal?) ;; equ? はこの段階では未定義
+
--- sicp/ddp.scm
+++ sicp/ddp.scm
@@ -410,46 +410,46 @@
   (define (adjoin-term term term-list)
     (if (=zero? (coeff term))
       term-list
-      (cons term (term-list))))
+      (cons term term-list)))
 
   (define (first-term term-list) (car term-list))
   (define (rest-terms term-list) (cdr term-list))
   (define (make-term order coeff) (list order coeff))
   (define (order term) (car term))
-  (define (coeff term) (cdr term))
+  (define (coeff term) (cadr term))
 
   (define (add-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
       (make-poly (variable p1)
                  (add-terms (term-list p1)
                             (term-list p2)))
-      (error "Polyqs not in same var -- ADD-POLY"
+      (error "polyqs not in same var -- add-poly"
         (list p1 p2))))
 
-  (define (add-terms L1 L2)
-    (cond [(empty-termlist? L1) L2]
-          [(empty-termlist? L2) L1]
+  (define (add-terms l1 l2)
+    (cond [(empty-termlist? l1) l2]
+          [(empty-termlist? l2) l1]
           [else
-           (let ([t1 (first-term L1)]
-                 [t2 (first-term L2)])
+           (let ([t1 (first-term l1)]
+                 [t2 (first-term l2)])
              (cond [(> (order t1) (order t2))
                     (adjoin-term t1
-                                 (add-terms (rest-terms L1) L2))]
+                                 (add-terms (rest-terms l1) l2))]
                    [(< (order t1) (order t2))
                     (adjoin-term t2
-                                 (add-terms L1 (rest-terms L2)))]
+                                 (add-terms l1 (rest-terms l2)))]
                    [else
                     (adjoin-term (make-term (order t1)
                                             (add (coeff t1) (coeff t2)))
-                                 (add-terms (rest-terms L1)
-                                            (rest-terms L2)))]))]))
+                                 (add-terms (rest-terms l1)
+                                            (rest-terms l2)))]))]))
 
   (define (mul-poly p1 p2)
     (if (same-variable? (variable p1) (variable p2))
       (make-poly (variable p1)
                  (mul-terms (term-list p1)
                             (term-list p2)))
-      (error "Polys not in same var -- MUL-POLY"
+      (error "polys not in same var -- mul-poly"
         (list p1 p2))))
 
   (define (mul-terms L1 L2)
@@ -468,14 +468,16 @@
 
   ;; システムの他の部分とのインターフェース
   (define (tag p) (attach-tag 'polynomial p))
-  (put 'add '(polynomial polynomial)
-       (^ (p1 p2) (tag (add-poly p1 p2))))
-  (put 'mul '(polynomial polynomial)
-       (^ (p1 p2) (tag (mul-poly p1 p2))))
-  (put 'make 'polynomial
-       (^ (var terms) (tag (make-poly var terms))))
+  (put-method 'add '(polynomial polynomial)
+              (^ (p1 p2) (tag (add-poly p1 p2))))
+  (put-method 'mul '(polynomial polynomial)
+              (^ (p1 p2) (tag (mul-poly p1 p2))))
+  (put-method 'make 'polynomial
+              (^ (var terms) (tag (make-poly var terms))))
 
   'done)
 
+(install-polynomial-package)
+
 (define (make-polynomial var terms)
-  ((get 'make 'polynomial) var terms))
+  ((get-method 'make 'polynomial) var terms))
