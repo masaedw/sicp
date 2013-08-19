bfe3cc91f734e3dedaed1dbc88f13ea957a65958
masaedw <masa.edw@gmail.com>
2013-08-19 23:13:38 +0900

    別ファイルに依存しないように

--- 1.21.scm
+++ 1.21.scm
@@ -1,5 +1,16 @@
-(add-load-path ".")
-(use sicp)
+(define (square n)
+  (* n n))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b)
+  (= (remainder b a) 0))
 
 (define (main args)
   (print #`"The smallest divisor of 199 is ,(smallest-divisor 199)")
