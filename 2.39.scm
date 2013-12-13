c59b51f7756354afe8072dabc5e445df5c427e96
masaedw <masa.edw@gmail.com>
2013-12-13 20:15:44 +0900

    2.39

--- 2.39.scm
+++ 2.39.scm
@@ -1,12 +1,18 @@
 (add-load-path ".")
-(use SICP)
+(use sicp)
 
 (define (reverser sequence)
-  (fold-right (lambda (x y) (cons x y)) nil sequence))
+  (fold-right (lambda (x y) (append y (list x))) nil sequence))
 
 (define (reversel sequence)
-  (fold-left (lambda (x y) (append x (list y))) nil sequence))
+  (fold-left (lambda (x y) (cons y x)) nil sequence))
 
 (define (main args)
   (p (reverser '(1 2 3 4 5 6))
      (reversel '(1 2 3 4 5 6))))
+
+;; right
+;; (1 ? (2 ? (3 ? nil)))
+;;
+;; left
+;; (((nil ? 1) ? 2) ? 3)
