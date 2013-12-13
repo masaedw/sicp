884f8cbf3a184e956aa838d99b154859cdbfc4c2
masaedw <masa.edw@gmail.com>
2013-12-13 20:03:44 +0900

    update 2.38

--- 2.38.scm
+++ 2.38.scm
@@ -1,5 +1,5 @@
 (add-load-path ".")
-(use SICP)
+(use sicp)
 
 (define (fold-left op initial sequence)
   (define (iter result rest)
@@ -12,10 +12,15 @@
 (define fold-right accumulate)
 
 (define (main args)
-  (p (fold-right / 1 (list 1 2 3))
-     (fold-left / 1 (list 1 2 3))
+  (p (fold-right /. 1 (list 1 2 3))
+     (fold-left /. 1 (list 1 2 3))
      (fold-right list nil (list 1 2 3))
      (fold-left list nil (list 1 2 3))))
 
-;; fold-right と fold-left が同じ結果になるために op が満たすべき性質
-;; (op a b) == (op b a) となること
+;; fold-right と fold-left が同じ結果になるために op が満たすべき性質は何か？
+;;
+;; fold-right は、右結合でオペレータを適用する操作である
+;; (((a op b) op c) op d)
+;; fold-left は、左結合でオペレータを適用する操作である
+;; (a op (b op (c op d)))
+;; よって、どちらも同じ結果になるために、opが満たすべき性質は、結合法則である。
